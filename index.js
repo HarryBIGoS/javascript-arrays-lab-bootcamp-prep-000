cd85a8beef8410c7c5b7a4079ef9b3c6a6a66b27
Harry Bigos <hrbigos@gmail.com>
2019-01-13 20:48:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,14 +48,12 @@ function prependKitten(name) {
 
 
 function removeLastKitten() {
-  kittens.slice(2);
-  return kittens
+ return kittens.slice(0, kittens.length - 1);
 }
 
 
 
 
 function removeFirstKitten() {
-kittens.slice(0);
-return kittens
+return kittens.slice(1);
 }
